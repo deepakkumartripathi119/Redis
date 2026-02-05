d150526ae675b72247e73c6f64025a6d19c41943
Deepak Kumar Tripathi <deepaktripathi14022005@gmail.com>
2026-02-05 23:54:36 +0530

    test

---
+++ src/main/java/GlobeStore.java
@@ -0,0 +1,8 @@
+import java.util.concurrent.ConcurrentHashMap;
+
+public class GlobeStore {
+    // 1. 'public' means everyone can see it
+    // 2. 'static' means there is only ONE copy for the whole program
+    // 3. 'final' ensures you don't accidentally replace the map with a new one
+    public static final ConcurrentHashMap<String, String> data = new ConcurrentHashMap<>();
+}
--- src/main/java/RequestParser.java
+++ src/main/java/RequestParser.java
@@ -1,20 +1,32 @@
 import java.sql.SQLOutput;
+import java.util.HashMap;
 
 public class RequestParser {
-    public String requestParser(String input) {
-        System.out.println("input: " + input);
+    public String requestParser(String input)
+    {
         if (!input.startsWith("*")) {
             return "error: expected '*'\r\n";
         }
         String[] chunks = input.split("\r\n");
-        String output = "";
+        return requestProcessor(chunks);
+    }
+    public String requestProcessor(String[] chunks) {
         if (chunks.length >= 5 && chunks[2].equalsIgnoreCase("ECHO")) {
             String data = chunks[4];
-            output = output.concat("$" + data.length() + "\r\n" + data + "\r\n");
-        } else {
-            output = output.concat("+PONG\r\n");
+            return ("$" + data.length() + "\r\n" + data + "\r\n");
+        } else if (chunks[2].equalsIgnoreCase("PING")) {
+            return ("+PONG\r\n");
+        } else if (chunks.length >= 7 && chunks[2].equalsIgnoreCase("SET")) {
+            String key = chunks[4];
+            String val = chunks[6];
+            GlobeStore.data.put(key,val);
+            return ("+OK\r\n");
+        } else if (chunks.length >= 5 && chunks[2].equalsIgnoreCase("GET")) {
+            String key = chunks[4];
+            String val = GlobeStore.data.get(key);
+            if(val.isEmpty())return "$-1\r\n";
+            return ("$" + val.length() + "\r\n" + val + "\r\n");
         }
-
-        return output;
+        return "$-1\r\n";
     }
 }
