20ec95c85c1426df81f395746285fecc5d8888ca
Deepak Kumar Tripathi <deepaktripathi14022005@gmail.com>
2026-02-06 03:35:25 +0530

    test

--- src/main/java/GlobeStore.java
+++ src/main/java/GlobeStore.java
@@ -1,3 +1,4 @@
+import java.time.Instant;
 import java.util.concurrent.ConcurrentHashMap;
 
 public class GlobeStore {
@@ -5,4 +6,5 @@ public class GlobeStore {
     // 2. 'static' means there is only ONE copy for the whole program
     // 3. 'final' ensures you don't accidentally replace the map with a new one
     public static final ConcurrentHashMap<String, String> data = new ConcurrentHashMap<>();
+    public static final ConcurrentHashMap<String, Instant> expTime = new ConcurrentHashMap<>();
 }
--- src/main/java/RequestParser.java
+++ src/main/java/RequestParser.java
@@ -1,15 +1,16 @@
 import java.sql.SQLOutput;
+import java.time.Instant;
 import java.util.HashMap;
 
 public class RequestParser {
-    public String requestParser(String input)
-    {
+    public String requestParser(String input) {
         if (!input.startsWith("*")) {
             return "error: expected '*'\r\n";
         }
         String[] chunks = input.split("\r\n");
         return requestProcessor(chunks);
     }
+
     public String requestProcessor(String[] chunks) {
         if (chunks.length >= 5 && chunks[2].equalsIgnoreCase("ECHO")) {
             String data = chunks[4];
@@ -19,12 +20,29 @@ public class RequestParser {
         } else if (chunks.length >= 7 && chunks[2].equalsIgnoreCase("SET")) {
             String key = chunks[4];
             String val = chunks[6];
-            GlobeStore.data.put(key,val);
+            GlobeStore.data.put(key, val);
+            if (chunks.length >= 11 && chunks[8].equalsIgnoreCase("PX")) {
+                String exp = chunks[10];
+                long millSec = Long.parseLong(exp);
+                GlobeStore.expTime.put(key, Instant.now().plusMillis(millSec));
+            }
+            else if(chunks.length >= 11 && chunks[8].equalsIgnoreCase("EX"))
+            {
+                String exp = chunks[10];
+                long sec = Long.parseLong(exp);
+                GlobeStore.expTime.put(key, Instant.now().plusSeconds(sec));
+            }
             return ("+OK\r\n");
         } else if (chunks.length >= 5 && chunks[2].equalsIgnoreCase("GET")) {
             String key = chunks[4];
             String val = GlobeStore.data.get(key);
-            if(val.isEmpty())return "$-1\r\n";
+            if (val.isEmpty()) return "$-1\r\n";
+            Instant expiry = GlobeStore.expTime.get(key);
+            if(expiry!=null&&Instant.now().isAfter(expiry))
+            {
+                GlobeStore.expTime.remove(key);
+                return "$-1\r\n";
+            }
             return ("$" + val.length() + "\r\n" + val + "\r\n");
         }
         return "$-1\r\n";
