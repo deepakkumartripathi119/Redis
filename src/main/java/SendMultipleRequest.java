a5e22ac46009495933de28805b11b05daa0ebbac
Deepak Kumar Tripathi <deepaktripathi14022005@gmail.com>
2026-02-05 18:09:46 +0530

    test

---
+++ src/main/java/SendMultipleRequest.java
@@ -0,0 +1,28 @@
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.net.Socket;
+
+public class SendMultipleRequest {
+
+    void multipleResponse(Socket clientSocket)
+    {
+        try {
+            OutputStream outputStream = clientSocket.getOutputStream();
+            InputStream inputStream  = clientSocket.getInputStream();
+
+            while(true)
+            {
+                byte[] input = new byte[1024];
+                int byteCount = inputStream.read(input);
+                String inputString = new String(input).trim();
+
+                outputStream.write("+PONG\r\n".getBytes());
+                outputStream.flush();
+
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+}
